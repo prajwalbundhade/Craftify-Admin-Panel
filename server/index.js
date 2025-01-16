967b0e40fe649aa02a76c71485e5bd64a9e8c908
prajwalbundhade <prajwalpro231@gmail.com>
2025-01-17 00:43:01 +0530

    path fixed for api's

--- server/index.js
+++ server/index.js
@@ -12,29 +12,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 const cors = require('cors');
-const allowedOrigins = [
-    'https://craftifyservice.online', 
-    'http://craftifyservice.online',
-     'https://www.craftifyservice.online',
-     'https://dashboard.craftifyservice.online', 
-    'http://dashboard.craftifyservice.online',
-     'https://www.dashboard.craftifyservice.online'   
-  ];
-  
-  const corsOptions = {
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {  // !origin allows Postman/CLI tools to make requests
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST'],
-    credentials: true,
-  };
-  
-  // Enable CORS with the options
-  app.use(cors(corsOptions));
+
+// Allow all origins
+app.use(cors({
+    origin: '*',  // Allows all origins
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],  // Allows these specific methods
+    credentials: true,  // If you need to allow credentials like cookies
+  }));
 
 // Routes
 app.use("/api", authRoutes);
